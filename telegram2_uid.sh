3e4eb312c97a39e34f2352833ed13a71aaab3f5c
Sebastian <servidge@users.noreply.github.com>
2017-03-29 22:11:59 +0200

    Update telegram2_uid.sh
    add Errorcode

--- telegram2_uid.sh
+++ telegram2_uid.sh
@@ -6,7 +6,12 @@ TIMEOUT="11"
 URL="https://api.telegram.org/bot$APITOKEN/sendMessage"
 EXECDAT="$(date "+%Y-%m-%d %H:%M")"
 MSGTEXT="$EXECDAT - Message: $*"
+ERRORSUM="0"
+ERRORCODE="0"
 for USERID in `echo $USERIDS`
 do
     curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$MSGTEXT" $URL > /dev/null
+ERRORCODE=$?
+ERRORSUM=$(($ERRORSUM+$ERRORCODE))
 done
+exit $ERRORSUM
