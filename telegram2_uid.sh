15c9de5e5048713720f45bc69f19886423fceb98
Sebastian <servidge@users.noreply.github.com>
2017-03-29 21:50:41 +0200

    Create telegram2_uid.sh

---
+++ telegram2_uid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Send telegram Messages to one or several USERID from Bot.
+USERIDS="<USERID> <USERID2> <USERID3>"
+APITOKEN="<TELEGRAMTOKEN>"
+TIMEOUT="11"
+URL="https://api.telegram.org/bot$APITOKEN/sendMessage"
+EXECDAT="$(date "+%Y-%m-%d %H:%M")"
+MSGTEXT="$EXECDAT - Message: $*"
+for USERID in `echo $USERIDS`
+do
+    curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$MSGTEXT" $URL > /dev/null
+done
