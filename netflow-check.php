685d9ea92d45b1ed940629cccc2e29a8a23323a6
Sebastian <news.sb@web.de>
2017-11-30 21:36:16 +0100

    first
    initial

---
+++ netflow-check.php
@@ -0,0 +1,61 @@
+﻿<?php
+# Der größte Teil des Quellcode ist von "Jimmy" http://blog.cactiusers.org/2014/06/06/decoding-netflow-v5-packets-with-php/
+# Keine Lizenzangabe. Aber auf "... Now, for anyone wanting to write their own Netflow parser in PHP, I have included some basic code below to get you started. ..." lässt sich wohl aufbauen.
+# Nur kleinere Anpassungen zum schnellen Check ob Netflow v5 Pakete am Collector ankommen. 
+# Nur CLI Ausgabe. Keine weitere Verarbeitung. 
+# äöüß?# ;)
+#
+# Most of the source code is from "Jimmy" http://blog.cactiusers.org/2014/06/06/decoding-netflow-v5-packets-with-php/
+# No license information. But the "... Now, for anyone wanting to write their own Netflow parser in PHP, I have included some basic code below to get you started. ..." seems to be a good foundation.
+# Just small adjustments to quickly check whether Netflow v5 packets arrive at the Collector. 
+# Only CLI output. No further processing.
+#
+# <Start Original from http://blog.cactiusers.org/2014/06/06/decoding-netflow-v5-packets-with-php/>
+#<?php
+# We first setup our listening UDP Socket.  We use the default port of 2055, but that can be changed to anything
+$socket = stream_socket_server("udp://0.0.0.0:2055", $errno, $errstr, STREAM_SERVER_BIND);
+if (!$socket) {
+    die("$errstr ($errno)");
+}
+ 
+do {
+ # We now wait for a packet.  Standard MTU is 1500.  
+ # I will need to check on if this needs to be moved to 9000+ for VMWare instances using Jumbo Packets.
+ $p = stream_socket_recvfrom($socket, 1500, 0, $peer);
+ 
+ # Packet Received, so we must first check if it is version 5
+ # This is were you would check to see if it is v9 or v10 also to decode those types of packets
+ $version = unpack("n", substr($p, 0, 2));
+ if ($version[1] == 5) {
+fv5($p, $peer);
+}
+} while ($p !== false);
+ 
+function fv5 ($p, $peer) {
+ # Packet Reference: http://www.cisco.com/c/en/us/td/docs/net_mgmt/netflow_collection_engine/3-6/user/guide/format.html#wp1006108
+ # The headers / flowrecs in v5 packets are fixed lengths, so we set them here
+ $v5_header_len = 24;
+ $v5_flowrec_len = 48;
+ 
+ # We now need to take a look at the Netflow header.  This will give us a nice array of named values, I don't make use of most of them in this example
+ $header = unpack('nversion/ncount/Nsysuptime/Nunix_secs/Nunix_nsecs/Nflow_sequence/Cengine_type/Cengine_id/nsampling_interval', substr($p, 0, 24));
+ 
+ # For now, we will just care about the count, as that will tell us how many flows are contained in this packet
+ $count = $header['count'];
+ 
+ # We will loop through each Netflow record here
+ for ($i = 0; $i < $count; $i++) {
+ # We need to pull the flow record out.  Since every flow is 48 bytes, and the header is 24 bytes,
+ # its pretty simple to determine the location of each flow record.
+ $flowrec = substr($p, $v5_header_len + ($i * $v5_flowrec_len), $v5_flowrec_len);
+ 
+ # We now will unpack the flow record into an array that is more usable
+ # For a rundown on what each field is, check the reference URL above.  They are named exactly the same
+ $flowdata = unpack('C4srcaddr/C4dstaddr/C4nexthop/ninput/noutput/NdPkts/NdOctets/NFirst/NLast/nsrcport/ndstport/Cblank/Ctcp_flags/Cprot/nsrc_as/ndst_as/Csrc_mask/Cdst_mask', $flowrec);
+ 
+ #
+ # You now have the entire netflow record in a tidy named array.  From here you can manipulate it however you like.  
+ # I personally just insert it into a Memory table in a MySQL database for later processing.
+ #
+ }
+}
