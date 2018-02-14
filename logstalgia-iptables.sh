fc14af3d7077494a83f6c5e65be80f3eea31c685
Sebastian <news.sb@web.de>
2018-02-14 22:43:29 +0100

    doku

--- logstalgia-iptables.sh
+++ logstalgia-iptables.sh
@@ -4,7 +4,20 @@
 # logstalgia.io "..website traffic visualization..." 
 # Part of https://github.com/servidge/snowflakes
 # äöüß
+# iptableskonfig
+# -A logaccept -m state --state NEW -j LOG --log-prefix "iptables-accept "
+# -A logaccept -j ACCEPT
+# -A logdrop -j LOG --log-prefix "iptables-drop "
+# -A logdrop -j DROP
+# -A rejectlog -j LOG --log-prefix "iptables-reject "
+# -A rejectlog -j REJECT --reject-with icmp-port-unreachable
+# Logs:
+#2018-02-14T22:36:37.851865+01:00 HOSTNAME kernel: [83172.422084] iptables-accept IN=eth11 OUT= MAC=aa:aa:aa:aa:aa:aa:aa:cc:cc:cc:cc:cc:cc:cc SRC=192.0.2.123 DST=198.51.100.11 LEN=219 TOS=0x00 PREC=0x00 TTL=251 ID=44695 PROTO=UDP SPT=56852 DPT=514 LEN=199 
+#2018-02-14T22:37:27.872349+01:00 HOSTNAME kernel: [83282.443510] iptables-drop IN=eth11 OUT= MAC=aa:aa:aa:aa:aa:aa:aa:cc:cc:cc:cc:cc:cc:cc SRC=192.0.2.122 DST=198.51.100.11 LEN=40 TOS=0x00 PREC=0x00 TTL=56 ID=33017 PROTO=TCP SPT=3288 DPT=23 WINDOW=15080 RES=0x00 SYN URGP=0 
+#2018-02-14T21:46:37.425024+01:00 HOSTNAME kernel: [83871.937830] iptables-reject IN=eth11 OUT= MAC=aa:aa:aa:aa:aa:aa:aa:cc:cc:cc:cc:cc:cc:cc SRC=192.0.2.110 DST=198.51.100.11 LEN=52 TOS=0x02 PREC=0x00 TTL=116 ID=3495 DF PROTO=TCP SPT=10216 DPT=22 WINDOW=8192 RES=0x00 CWR ECE SYN URGP=0 
 #
+#
+
 AUSGABE=""
 f_transform(){
 ZEILE=$@
@@ -32,7 +45,7 @@ else
 			f_transform ${line}
 		done < ${1} 
 	else
-		echo "No input given!"
+		echo "Nichts angegeben!"
 	fi
 fi
 
