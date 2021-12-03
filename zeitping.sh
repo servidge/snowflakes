d03102f04a83535391cc418d87f3d9e6adf9f954
Sebastian <servidge@users.noreply.github.com>
2021-12-03 22:31:37 +0100

    Update zeitping.sh
    komplizierter

--- zeitping.sh
+++ zeitping.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-# Umsetzung vom Script
+# Umsetzung von Sebastian Boenning
+# Part of https://github.com/servidge/snowflakes
 # Ping mit zeitstempel
-# von Sebastian Boenning
 echo "$(date +%Y-%m-%d_%T) | start Ping auf $1"
-ping -A -n $1 | while read pong; do echo $(date +%Y-%m-%d_%T) \| $pong;done 
-
+alttime=0
+altttl=0
+altseq=0
+echo "Zeitstempel | Pingoutput | Zeitvarianz zu vorher | TTL Aenderung | Pingverlust zu vorherigem "
+ping -A -n $1 | while read pong; do
+if [[ $pong != *"icmp_seq="* ]]; then
+	echo $(date +%Y-%m-%d_%T) \| $pong 
+else
+	time=`echo $pong | cut -d":" -f2 | cut -d" " -f4 | cut -d"=" -f2`
+	ttl=`echo $pong | cut -d":" -f2 | cut -d" " -f3 | cut -d"=" -f2`
+	seq=`echo $pong | cut -d":" -f2 | cut -d" " -f2 | cut -d"=" -f2`
+	
+	divtime=$( echo $time-$alttime | bc)
+	if [[ $divtime != *"-"* ]]; then
+		divtime="+$divtime"
+	fi
+	divttl=$((ttl-$altttl))
+	divseq=$((seq-$altseq-1))
+	#echo $(date +%Y-%m-%d_%T) \| $pong \| $time \| $ttl \| $seq \| $divtime \| $divttl \| $divseq
+	echo $(date +%Y-%m-%d_%T) \| $pong  \| $divttl \| $divseq \| $divtime
+	alttime=$time
+	altttl=$ttl
+	altseq=$seq  
+fi
+done
